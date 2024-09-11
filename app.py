233b1931dfd15efa0d4b1a359d3daddec312e76c
titi121164 <tcabre@free.fr>
2024-09-11 14:42:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ import streamlit as st
 import pandas as pd
 #
 st.title("My Dashboard TCE") 
+# Lecture du fichier 
 df=pd.read_csv('data.csv')
 
+if st.checkbox('Affciher le jeu de donnée'):
+# affichage Data Frame 
+  st.write(df)
+# 
 #
-st.write(df)
 
 
