f2585801dd01966dd7fed7d20f7256b3a2c13ed4
titi121164 <tcabre@free.fr>
2024-09-11 16:25:03 +0200

    Update graphique.py

--- pages/graphique.py
+++ pages/graphique.py
@@ -1,1 +1,16 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+#
+#
+# bouton upload file 
+
+uploaded_file = st.file_uploader("Choose a file",type='csv')
+if uploaded_file is not None:
+        # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+    liste_colonne = dataframe.columns
+    selection_col = st.selectbox('Sélectionner un axe ',liste_colonne)
+  ## Create a histogram of the 'age' column
 #
