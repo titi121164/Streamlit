fd0611cd128e0a0516eb70272ee1ed6993cdfaaf
titi121164 <tcabre@free.fr>
2024-09-11 16:38:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,17 +28,6 @@ if st.checkbox('Afficher le jeu de donnée'):
 #
 # bouton upload file 
 
-uploaded_file = st.file_uploader("Choose a file",type='csv')
-if uploaded_file is not None:
-        # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
-    liste_colonne = dataframe.columns
-    selection_col = st.selectbox('Sélectionner un axe ',liste_colonne)
-  ## Create a histogram of the 'age' column
-
-#  
-
 
 
 # Display the histogram
