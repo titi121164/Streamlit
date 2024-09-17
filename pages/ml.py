7a00f3aa7b2f606f35ca4c48e659595a37fb7e97
titi121164 <tcabre@free.fr>
2024-09-17 15:53:38 +0200

    Update ml.py

--- pages/ml.py
+++ pages/ml.py
@@ -2,6 +2,6 @@ import streamlit as st
 with st.form("my_form"):
   st.write("my form")
   name = st.text_input("ton nom ?")
-  submitted = st.form-submit_button("submit")
+  submitted = st.form_submit_button("submit")
   if submitted:
     st.write(f"hello {name}")
