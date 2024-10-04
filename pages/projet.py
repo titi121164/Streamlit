eeab1c40ccfdeebda148b9bf4bdf2aab963e5128
titi121164 <tcabre@free.fr>
2024-10-04 09:24:10 +0200

    Update projet.py

--- pages/projet.py
+++ pages/projet.py
@@ -1,5 +1,8 @@
 import streamlit as st
- 
+
+# URL de l'API FastAPI
+API_URL = "http://localhost:8000/insert_data"
+
 st.title("FAQ Streamlit Thierry")
  
 # Initialize chat history
@@ -12,10 +15,16 @@ if prompt := st.chat_input("What is up?"):
         st.markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
- 
+ #
+ # Appel à la méthode insert_data de FastAPI
+ response = requests.post(API_URL, json={"libelle": prompt})
+
 response = f"Echo: {prompt}"
-# Display assistant response in chat message container
-with st.chat_message("assistant"):
-    st.markdown(response)
+# Affichage de la réponse
+ if response.json()["status"] == "ok":
+   with st.chat_message("assistant"):
+     st.markdown(response)
+ else:
+     st.markdown(f"Erreur lors de l'insertion : {response.json()['reponse_openai']}")
 # Add assistant response to chat history
 st.session_state.messages.append({"role": "assistant", "content": response})
